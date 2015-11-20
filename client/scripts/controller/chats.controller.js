f5caf01c39355916d03b3c38ad3c1c6d6abbfc92
Kamalpreet Chauhan <kamalpreet.chauhan@advancedcomputersoftware.com>
2015-11-20 18:14:17 +0000

    #73653 Compelete step 6

--- client/scripts/controller/chats.controller.js
+++ client/scripts/controller/chats.controller.js
@@ -2,10 +2,26 @@ angular
   .module('Whatsapp')
   .controller('ChatsCtrl', ChatsCtrl);
  
-function ChatsCtrl ($scope) {
+function ChatsCtrl ($scope, $ionicModal, $meteor) {
   $scope.chats = $scope.$meteorCollection(Chats, false);
   
+  $ionicModal.fromTemplateUrl('client/templates/new-chat.html', {
+    scope: $scope
+  }).then(function (modal) {
+    $scope.modal = modal;
+  });
+ 
+  $scope.$on('$destroy', function () {
+    $scope.modal.remove();
+  });
+ 
+  $scope.openNewChatModal = openNewChatModal;
+  
   $scope.remove = function(chat){
-    $scope.chats.remove(chat);
+   $meteor.call('removeChat', chat._id);
+  }
+  
+  function openNewChatModal () {
+    $scope.modal.show();
   }
 }
